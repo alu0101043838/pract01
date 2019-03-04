66788aa4006b9e96882997024c8b9b4d6a31ad3e
alu0101043838 <alu0101043838@ull.edu.es>
2019-03-04 17:40:52 +0000

    networking with sockets

---
+++ networking/net-watcher-json-client.js
@@ -0,0 +1,17 @@
+/** net-watcher-json-client.js
+* @author Edgar Figueroa González - Grado en Ingeniería Informática ULL
+*/
+'use strict';
+const net = require('net');
+const client = net.connect({port: 60300});
+client.on('data', data => {
+  const message = JSON.parse(data);
+  if(message.type === 'watching'){
+    console.log(`Now watching: ${message.file}`);
+  } else if(message.type === 'changed'){
+    const date = new Date(message.timestamp);
+    console.log(`File changed: ${date}`);
+  } else {
+    console.log(`Unrecognized message type: ${message.type}`);
+  }
+});
