c3b29484638a45a557c9d8264cf131a8256d914c
alu0101043838 <alu0101043838@ull.edu.es>
2019-03-09 14:30:23 +0000

    networking with sockets

--- networking/lib/ldj-client.js
+++ networking/lib/ldj-client.js
@@ -6,6 +6,8 @@
 const EventEmitter = require('events').EventEmitter;
 class LDJClient extends EventEmitter {
   constructor(stream){
+    if (stream === null)
+      throw new Error('Stream is null');
     super();
     let buffer = '';
     stream.on('data', data => {
@@ -18,7 +20,22 @@ class LDJClient extends EventEmitter {
         boundary = buffer.indexOf('\n');
       }
     });
-  }
+
+  stream.on('close', () => {
+      let boundary = buffer.indexOf('}');
+      if(boundary !== -1){
+        const input = buffer.substring(0, boundary+1);
+         try {
+            this.emit('message', JSON.parse(input));
+         } catch (e) {
+            throw new Error('Non JSON message');
+         }
+      } else {
+          buffer = '';
+      }
+          this.emit('close');
+  });
+}
 
   static connect(stream){
     return new LDJClient(stream);
--- networking/test/ldj-client-test.js
+++ networking/test/ldj-client-test.js
@@ -17,10 +17,44 @@ describe('LDJClient', () => {
   });
 
   it('should emit a message event from a single data event', done => {
-    client.on('message', message => {
-      assert.deepEqual(message, {foo: 'bar'});
-      done();
+  	client.on('message', message => {
+  	   assert.deepEqual(message, {foo: 'bar'});
+  	   done();
+  	});
+  		stream.emit('data', '{"foo":');
+  		process.nextTick(() => stream.emit('data', '"bar"}\n'));
+  	});
+
+  it('should emit a message from split data events', done => {
+  	client.on('message', message => {
+  	   assert.deepEqual(message, {foo: 'bar' });
+  	   done();
+  	});
+  		 stream.emit('data', '{"foo');
+  		 process.nextTick(() => stream.emit('data', '": "bar"}'));
+  		 process.nextTick(() => stream.emit('data', '\n'));
+  });
+
+  it('null to LDJClient constructor', done => {
+  	assert.throws(() => {
+  	   new LDJClient(null);
+  	});
+  		 done();
+  });
+
+  it('should emit a data event that is not JSON', done => {
+    assert.throws(() => {
+      stream.emit('data', '{"foo\n');
     });
-    stream.emit('data', '{"foo":"bar"}\n');
+      done();
+  });
+
+  it('should show messages without newline followed by a close event', done => {
+		client.on('message', message => {
+			assert.deepEqual(message, {foo: 'bar' });
+			done();
+		});
+		  stream.emit('data', '{"foo": "bar"}');
+		  stream.emit('close');
   });
 });
